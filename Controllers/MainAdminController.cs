aa2f776777b28d0860646dec76fafd2e096dbecd
Ahsan Malik <m.ahsan.malik951@gmail.com>
2025-12-14 15:17:03 +0500

    Add appstirng.json

--- Controllers/MainAdminController.cs
+++ Controllers/MainAdminController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SRMS_APIs.Database;
 
 namespace SRMS_APIs.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MainAdminController : ControllerBase
+    public class MainAdminController(DB dBContext) : ControllerBase
     {
+        [HttpGet("check-db")]
+        public IActionResult CheckDatabase()
+        {
+            try
+            {
+                var admin = dBContext.Biit_Administration.ToList();
+                return Ok($"Database connected successfully! Found {admin.Count} societies.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Database connection failed: {ex.Message}");
+            }
+        }
+
+
     }
 }
